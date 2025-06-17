af3b92acfb565bd594ceae2a1273d8546fb10c24
ollydmc <159478099+ollydmc@users.noreply.github.com>
2025-06-17 16:00:17 +0100

    Add files via upload

--- birthdaycounter.js
+++ birthdaycounter.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 15, 2025 00:00:00").getTime();
+var countDownDate = new Date("Sep 15, 2025 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
